f049fdfe1397e847d345f099794e444084ae862a
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-12-30 21:46:13 +0900

    今後増えるユーザーストーリーのテストケースの格納場所と分けられる様に構造化した

--- e2e/codecept.conf.js
+++ e2e/codecept.conf.js
@@ -9,7 +9,7 @@ setCommonPlugins();
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: "./*_test.js",
+  tests: "./tests/**/*_test.js",
   output: "./output",
   helpers: {
     Playwright: {
--- e2e/tests/business_processes/order_process_test.js
+++ e2e/tests/business_processes/order_process_test.js
@@ -1,10 +1,10 @@
-SuiteOf("smoke");
+SuiteOf("注文プロセスのテスト");
 
-Scenario("Webサイトを開きログインする", ({ I }) => {
+Scenario("ログインし、お弁当を注文し、お弁当を受け取る", ({ I }) => {
   I.amOnPage("/");
   I.click("ログインする");
   I.fillField("ユーザー名", "user1");
   I.fillField("パスワード", "super-strong-passphrase");
   I.click("ログイン");
-  I.see("user1");
+  I.see("user1 さん");
 });
